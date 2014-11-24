73c29b317bdb295a5d511f372b42c9774d9a74bb
Moises P. Sena <moisespsena@gmail.com>
2014-11-24 16:05:18 -0200

    Renamed sdag2.sdag2 to sdag2.core and setup updated

--- sdag2/__init__.py
+++ sdag2/__init__.py
@@ -1,7 +1,16 @@
-__author__ = "Moises P. Sena"
+'''
+sdag2 - Python Simple Directed Acyclic Graph whith Cicle Detector and
+TopoloGical sorter utilities
+
+Created on Feb 15, 2013
+
+@author: Moises P Sena
+'''
+
+__author__ = 'Moises P Sena <moisespsena@gmail.com>'
 __license__ = "BSD"
 __version__ = "1.0"
 __maintainer__ = "Moises P. Sena"
 __email__ = "moisespsena@gmail.com"
 
-from .sdag2 import *
+from .core import *
--- setup.py
+++ setup.py
@@ -1,41 +1,47 @@
 from distutils.cmd import Command
-from setuptools import setup
 import os
 import re
 import sys
 import shutil
 
+from setuptools import setup, find_packages
+
+
 THIS_PATH = os.path.abspath(os.path.realpath(os.path.dirname(__file__)))
+
 
 def pkg_by_version(name=""):
     return name + ("" if sys.version_info[0] < 3 else "3")
 
+
 def read(fname):
     with open(os.path.join(THIS_PATH, fname)) as f:
         return f.read()
-          
+
+
 class CleanCommand(Command):
     user_options = []
 
     def initialize_options(self):
         self.paths = []
-        
+
         for f in os.listdir(THIS_PATH):
             if re.search("(^(build|dist|__pycache__)$|\.egg-info)", f):
                 self.paths.append(f)
-            
+
         for root, dirs, files in os.walk(THIS_PATH):
             for f in files:
-                if f.endswith(".pyc") or f.endswith(".pyc") or f.endswith(".pyo"):
+                if f.endswith(".pyc") or f.endswith(".pyc") or f.endswith(
+                        ".pyo"):
                     self.paths.append(os.path.join(root, f))
-                    
+
             for f in dirs:
                 if re.search("^(__pycache__)$", f):
                     self.paths.append(os.path.join(root, f))
 
     def finalize_options(self):
         pass
-    
+
     def run(self):
         for p in self.paths:
             if os.path.exists(p):
@@ -45,20 +51,22 @@ class CleanCommand(Command):
                 else:
                     print("Remove file     : " + p)
                     os.unlink(p)
-print('tests.' + pkg_by_version("p") + ".test_all")
+
+
 setup(
-    name="py_sdag2",
+    name="sdag2",
     version="1.0",
     author="Moises P. Sena",
     author_email="moisespsena@gmail.com",
-    description=("Simple Directed Acyclic Graph whith Cicle Detector and TopoloGical sorter utilities."),
+    description=("Python Simple Directed Acyclic Graph whith Cicle Detector "
+                 "and TopoloGical sorter utilities."),
     license="BSD",
     keywords="utils utilities directed graph digraph topological simple dag",
     url="https://github.com/moisespsena/py_sdag2",
-    packages=["sdag2"],
+    packages=find_packages(),
     scripts=["scripts/tsort.py"],
     test_suite='tests.' + pkg_by_version("p") + ".test_all",
-    long_description=read('README.md'),
+    long_description=read('README.rst'),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Topic :: Utilities",
@@ -68,7 +76,6 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
     ],
     cmdclass={'clean': CleanCommand}
 )
